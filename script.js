3363dee99aabde6bbc5c7a45a032391dd3eafc62
Pedro Martins Falleiros <pmfrp@hotmail.com>
2021-06-22 23:47:07 -0300

    estudo

---
+++ script.js
@@ -0,0 +1,50 @@
+const btnSubmit = document.getElementById('submit');
+const form = document.getElementById('form');
+
+btnSubmit.addEventListener("click", event => {
+    event.preventDefault();
+    const fields = [...document.querySelectorAll('input')];
+    fields.forEach(field => {
+        if (field.value === '') {
+            form.classList.add('validate-error');
+        }
+    });
+
+    const formError = document.querySelector('.validate-error');
+    if (formError) {
+        formError.addEventListener('animationend', event => {
+            if (event.animationName === 'nono') {
+                formError.classList.remove('validate-error');
+            }
+        })
+    } else {
+        form.classList.add('form-hide');
+    }
+});
+
+form.addEventListener('animationend', event => {
+    if (event.animationName === 'moveDown') {
+        from.style.display = 'none';
+    }
+})
+
+const ulSquares = document.querySelector('ul.squares');
+
+for (let i = 0; i < 11; i++) {
+    const li = document.createElement('li');
+    const random = (min, max) => Math.random() * (max - min) + min;
+    const size = Math.floor(random(10, 140));
+    const position = random(1, 99);
+    const delay = random(5, 0.1);
+    const duration = random(24, 12);
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `-${size}px`;
+    li.style.left = `${position}%`;
+    li.style.animationDelay = `${delay}s`;
+    li.style.animationDuration = `${duration}s`;
+    li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
+
+    ulSquares.appendChild(li)
+
+}
